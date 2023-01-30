571a2bf7e92c28396df265721fd6f53998f64c4c
Vladimir Horev <VShorev@yandex.ru>
2023-01-30 16:28:09 +0300

    add mui

--- src/App.js
+++ src/App.js
@@ -3,42 +3,34 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 import Maininput from './components/Maininput';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   const [todo, setTodo] = useState([])
   const [edit, setEdit] = useState([])
   const [task, setTask] = useState([])
-  const [taskfiltered, setTaskfiltered] = useState([])
-
-
 
   useEffect(() => {
     fetch('https://todo.soprano.biz/note/')
       .then((response) => {
-        // Проверяем успешность запроса 
         if (response.status !== 200) { console.log(response.status); }
         return response.json()
       }
       )
       .then((data) => setTodo(data))
-
   }, [todo])
-
-
 
   useEffect(() => {
     fetch('https://todo.soprano.biz/task/')
       .then((response) => response.json())
       .then((data) => setTask(data))
-  }, [])
+  }, [task])
 
-  ///////////////////////////////
   const deletehandler = (id) => {
     fetch(`https://todo.soprano.biz/note/${id}`, {
       method: 'DELETE',
     }).then(
       (response) => {
-        // Проверяем, что сервер ответил ОК и только потом обновляем state нашего UI объекта
         if (response.status === 200) {
           fetch('https://todo.soprano.biz/note/')
             .then((data) => setTodo(data))
@@ -47,22 +39,15 @@ function App() {
     )
   }
 
-  /////////////////////////
   const deletetask = (id) => {
     fetch(`https://todo.soprano.biz/task/${id}`, {
       method: 'DELETE',
     })
     fetch('https://todo.soprano.biz/task')
-      .then(
-        (response) => {
-          // Проверяем, что сервер ответил ОК и только потом обновляем state нашего UI объекта
-          if (response.status === 200) {
-          }
-        }
+      .then((response) => { if (response.status === 200) { } }
       )
   }
 
-  //////////////////////////
   const checkbox = (id) => {
     let objtask = {
       subject: `done`,
@@ -75,25 +60,18 @@ function App() {
         fetch(url, {
           method: 'PUT',
           body: JSON.stringify(objtask),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          }
+          headers: { 'Content-type': 'application/json; charset=UTF-8', }
         })
           .then(
             (response) => {
-              // Проверяем, что сервер ответил ОК и только потом обновляем state нашего UI объекта
               if (response.status === 200) {
-                ////////////////////////////////// как то надо обновить ссотояние
                 setTask(task)
-                ////////////////////////////
-                console.log(response.status);
               }
             }
           )
       }
   }
 
-  ////////////////////////
   const addTask = (id) => {
     let objtask = {
       subject: edit,
@@ -107,25 +85,12 @@ function App() {
         headers: { 'Content-type': 'application/json; charset=UTF-8', },
       }
     )
-    //  надо получить массив с отфильтрованными таксками по id
-    let result = task.filter(item => {
-      if (id === item.note_id) { return true }
-    })
-    setTaskfiltered(result)
   }
 
   const handleChange = (event) => {
-
-
     let value = event.target.value;
     setEdit(value);
-
-    console.log(edit)
-
-    // setEdit(event.target.value);
   }
-
-
 
   return (
     <div>
@@ -138,50 +103,29 @@ function App() {
                 <span className='task'  >   {item.name}   </span>
                 <Button variant="text" onClick={() => deletehandler(item.id)}> delete Notes</Button> <br />
                 <input type="text" placeholder='add text for edit task' name={item.id} value={edit.name} onChange={handleChange} />
-                <Button onClick={() => addTask(item.id, item.note_id)} >  add task </Button>
-                <Button onClick={() => addTask(item.id, item.note_id)} >  Shows task </Button>
-
+                <Button onClick={() => addTask(item.id, item.note_id)} variant="contained" size='small'>  add task </Button>
                 {item.tasks.map((item) => {
-                return (
-                  <div key={item.id} className='board'>   {item.subject}
-                  </div>
-                )
-              }
-              )}
+                  return (
+                    <div key={item.id} className='board'>
+                      <input type="checkbox" checked={item.resolved} ></input>
+                      <span onClick={() => checkbox(item.id)} className='pointer' >  {item.subject} </span>
+                      {/* <Button variant="text" onClick={() => deletetask(item.id)}> delete task</Button> */}
+
+                      <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => deletetask(item.id)} size='small'>
+                        Delete task
+                      </Button>
+
+                    </div>
+                  )
+                })}
               </div>
-
-              {/* {item.tasks.map((item) => {
-                return (
-                  <div key={item.id} className='board'>   {item.subject}
-                  </div>
-                )
-              }
-              )} */}
-
-              {/* <div> {item.tasks[0].id} </div>
-              <div> {item.tasks[0].subject} </div>
-              <div> {item.tasks[1].subject} </div> */}
-
             </div>
           )
         })
         }
       </div>
 
-      <div className='position'>
-        <div> <h2> List of tasks</h2></div>{
-          taskfiltered.map((item) => {
-            return (
-              <div>
-                <div key={item.note_id} className='board'>
-                  <input type="checkbox" checked={item.resolved} ></input>
-                  <span onClick={() => checkbox(item.id)} className='pointer' >  {item.subject} </span>
-                  <Button variant="text" onClick={() => deletetask(item.id)}> delete task</Button>
-                </div>
-              </div>
-            )
-          })
-        }</div>
+
     </div >
   );
 }
